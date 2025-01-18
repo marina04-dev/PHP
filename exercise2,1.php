19dae77098fc9e9c6e71c4c15d51fdcb45bab664
marina04-dev <marinachatzeephraimidou@gmail.com>
2025-01-18 10:30:38 +0200

    Create exercise2,1.php

---
+++ exercise2,1.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Document</title>
+</head>
+<body>
+<?php
+  // Even Numbers 21 until 99
+  for ($i = 21; $i <= 99; $i += 2) {
+    echo $i;
+    echo '<hr style="width:5%;text-align:left;margin-left:0">';
+  }
+?>
+</body>
