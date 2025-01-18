42e3da62ccb13cb2dee4968219098bca924fb049
marina04-dev <marinachatzeephraimidou@gmail.com>
2025-01-18 11:23:41 +0200

    Create exercise 2_4.php

---
+++ exercise 2_4.php
@@ -0,0 +1,203 @@
+<!DOCTYPE html>
+<html lang="el">
+
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<meta http-equiv="X-UA-Compatible" content="ie=edge">
+	<title>Roots of Trinomials</title>
+</head>
+<!-- The code is repeated 3 times as the values are not user's 
+input , so that it is shown all 3 cases-->
+
+<body style="color:black;text-align:center;">
+	<div>
+		<div style="color:blue;text-align:left;">
+			<h2>Finding The Roots of a Trinomial</h2>
+			<h5 style="color:red;">
+				<b>(Positive Distinctive)</b>
+			</h5>
+
+		</div>
+		<div>
+			<!-- <h3>Trinomial</h3> -->
+			<p>
+				<u>Show up of the roots of the Trinomial:</u>
+			</p>
+		</div>
+		<!-- This is a trinomial -->
+		<div>
+			<p>
+				<strong>x
+					<sup>2</sup> - 5x + 6 = 0</strong>
+			</p>
+		</div>
+		<div>
+			<?php
+            $a = 1;
+            $b = -5;
+            $c = 6;                 
+                      
+            // Result of Distinctive
+            $distinctive_trinomial = (-$b)**2-4*$a*$c;
+            echo "<b>The Distinctive of the Trinomial is: </b>" . $distinctive_trinomial . "<br>";
+           
+
+            // 3 Cases for Roots
+            if ($distinctive_trinomial > 0) {
+                echo "<p><b>The roots of the Trinomial are: </b></p>";
+                $x11 = -$b+sqrt($distinctive_trinomial);
+                $x12 = 2*$a;
+                $x1 = $x11 / $x12;
+                echo "<p><b>x<sub>1</sub> = " . $x1 . "</b>" . "</p>";
+            
+                $x21 = -$b-sqrt($distinctive_trinomial);
+                $x22 = 2*$a;
+                $x2 = $x21 / $x22;
+                echo "<p><b>x<sub>2</sub> = " . $x2 . "</b>" . "</p>";
+            }
+            elseif ($distinctive_trinomial == 0) {          
+                echo "<p>The Zero Distinctive has one double Root</p>";
+                $x21 = -$b;
+                $x22 = 2*$a;
+                $x2 = $x21 / $x22;
+                echo "x<sub>1</sub> = " . $x2;
+            }
+            else {
+                echo "<p>Distinctive is Negative and there are no float roots</p>";
+            }              
+            ?>
+
+		</div>
+	</div>
+	<hr>
+
+	<!-- --------------------------------------------------------------------------------- -->
+	<div>
+		<div style="color:blue;text-align:left;">
+			<h2>Finding The Roots of the Trinomial</h2>
+			<h5 style="color:red;">
+				<b>(Zero Distinctive)</b>
+			</h5>
+
+		</div>
+		<div>
+			<!-- <h3>Trinomial</h3> -->
+			<p>
+				<u>Show up of the roots of the Trinomial:</u>
+			</p>
+		</div>
+		<!-- This is a trinomial -->
+		<div>
+			<p>
+				<strong>9x
+					<sup>2</sup> + 30x + 25 = 0</strong>
+			</p>
+		</div>
+		<div>
+			<?php
+            $a = 9;
+            $b = 30;
+            $c = 25;                 
+                      
+            // Result of Distinctive
+            $distinctive_trinomial = (-$b)**2-4*$a*$c;
+            echo "<b>The Distinctive of the Trinomial is: </b>" . $distinctive_trinomial . "<br>";
+           
+
+            // 3 Cases foor Roots
+            if ($distinctive_trinomial > 0) {
+                echo "<p><b>The roots of the Trinomial are: </b></p>";
+                $x11 = -$b+sqrt($distinctive_trinomial);
+                $x12 = 2*$a;
+                $x1 = $x11 / $x12;
+                echo "<p><b>x<sub>1</sub> = " . $x1 . "</b>" . "</p>";
+            
+                $x21 = -$b-sqrt($distinctive_trinomial);
+                $x22 = 2*$a;
+                $x2 = $x21 / $x22;
+                echo "<p><b>x<sub>2</sub> = " . $x2 . "</b>" . "</p>";
+            }
+            elseif ($distinctive_trinomial == 0) {          
+                echo "<p>The Zero Distinctive has one double Root</p>";
+                $x21 = -$b;
+                $x22 = 2*$a;
+                $x2 = $x21 / $x22;
+                echo "x<sub>1,2</sub> = " . $x2;
+            }
+            else {
+                echo "<p> The Distinctive is negative and there are no float roots</p>";
+            }              
+            ?>
+
+		</div>
+	</div>
+	<hr>
+
+	<!-- --------------------------------------------------------------------------------- -->
+	<div>
+		<div style="color:blue;text-align:left;">
+			<h2>Finding The Roots of The Trinomial</h2>
+			<h5 style="color:red;">
+				<b>(Negative Distinctive)</b>
+			</h5>
+
+		</div>
+		<div>
+			<!-- <h3>Trinomial</h3> -->
+			<p>
+				<u>Show up of the Roots of the Trinomial:</u>
+			</p>
+		</div>
+		<!-- This is a Trinomial -->
+		<div>
+			<p>
+				<strong>9x
+					<sup>2</sup> - 5x + 6 = 0</strong>
+			</p>
+		</div>
+		<div>
+			<?php
+            $a = 9;
+            $b = -5;
+            $c = 6;                 
+                      
+            // Result of Distinctive
+            $distinctive_trinomial = (-$b)**2-4*$a*$c;
+            echo "<b>The Distinctive of the Trinomial is: </b>" . $distinctive_trinomial . "<br>";
+           
+
+            // 3 Cases for Roots
+            if ($distinctive_trinomial > 0) {
+                echo "<p><b>The roots of the Trinomial are: </b></p>";
+                $x11 = -$b+sqrt($distinctive_trinomial);
+                $x12 = 2*$a;
+                $x1 = $x11 / $x12;
+                echo "<p><b>x<sub>1</sub> = " . $x1 . "</b>" . "</p>";
+            
+                $x21 = -$b-sqrt($distinctive_trinomial);
+                $x22 = 2*$a;
+                $x2 = $x21 / $x22;
+                echo "<p><b>x<sub>2</sub> = " . $x2 . "</b>" . "</p>";
+            }
+            elseif ($distinctive_trinomial == 0) {          
+                echo "<p>The Zero Distinctive has one double Root</p>";
+                $x21 = -$b;
+                $x22 = 2*$a;
+                $x2 = $x21 / $x22;
+                echo "x<sub>1,2</sub> = " . $x2;
+            }
+            else {
+                echo "<p><b><i><u> The Distinctive is Negative and there are no float roots</b></i></u></p>";
+            }              
+            ?>
+			<hr>
+
+		</div>
+	</div>
+	<br>
+	<br>
+	
+</body>
+
+</html>
